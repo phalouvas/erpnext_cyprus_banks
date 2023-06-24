dd525bdee29c97001d596da2d395dcf78d726ee2
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-06-24 04:18:33 +0000

    managed to save autorization code

--- erpnext_hellenic_bank/erpnext_hellenic_bank/doctype/settings/settings.js
+++ erpnext_hellenic_bank/erpnext_hellenic_bank/doctype/settings/settings.js
@@ -2,7 +2,40 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Settings', {
-	// refresh: function(frm) {
+	refresh: function (frm) {
 
-	// }
+		frm.add_custom_button(__('Authorization Code Request'), function () {
+			let urlParams = new URLSearchParams(window.location.search);
+			if (urlParams.get('state') === null) {
+				let is_sandbox = frm.get_field('is_sandbox').value;
+				let base_url = is_sandbox ? "https://sandbox-oauth.hellenicbank.com" : "https://oauthprod.hellenicbank.com";
+				let client_id = frm.get_field('client_id').value;
+				let client_secret = frm.get_field('client_secret').value;
+				let href = base_url + "/oauth2/auth?response_type=code&client_id=" + client_id +
+					"&redirect_uri=" + window.location.href +
+					"&scope=b2b.account.details,b2b.credit.transfer.mass,b2b.account.list,b2b.report.account.statements,b2b.credit.transfer.cancel,b2b.report.credit.transfer.single,b2b.credit.transfer.single,b2b.funds.availability,b2b.report.credit.transfer.mass" +
+					"&state=erpnext_state_b64_encoded";
+				window.location.href = href;
+				frappe.validated = true;
+			}
+		});
+
+		let urlParams = new URLSearchParams(window.location.search);
+		let new_code = urlParams.get('code');
+		if (new_code !== null) {
+			frappe.db.get_single_value('Settings', 'code')
+				.then(function (old_code) {
+					if ((urlParams.get('state') === "erpnext_state_b64_encoded") && (new_code !== old_code)) {
+						frappe.db.set_value('Settings', '', 'code', urlParams.get('code'))
+							.then(r => {
+								let doc = r.message;
+								frappe.msgprint("You succesfully received a new authorization code: " + new_code);
+							})
+					}
+				});
+		}
+
+
+
+	}
 });
--- erpnext_hellenic_bank/erpnext_hellenic_bank/doctype/settings/settings.py
+++ erpnext_hellenic_bank/erpnext_hellenic_bank/doctype/settings/settings.py
@@ -1,8 +1,40 @@
 # Copyright (c) 2023, KAINOTOMO PH LTD and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+import base64
+import requests
 
 class Settings(Document):
+
+	def onload(self):
+		return
+		if (get_token(self) == False):
+			frappe.throw("Cannot connect")
+
+	pass
+
+def get_base_url(settings):
+	return "https://sandbox-oauth.hellenicbank.com" if settings.is_sandbox else "https://oauthprod.hellenicbank.com"
+
+def get_token(settings):
+	url = get_base_url(settings) + "/token/exchange"
+	payload = {
+		"grant_type": "authorization_code",
+		"redirect_uri": frappe.local.request.url,
+		"code": settings.code
+	}
+	string_to_encode = settings.client_id + ':' + settings.client_secret
+	headers = {
+		"Authorization": "Basic " + base64.b64encode(string_to_encode.encode("utf-8")).decode("utf-8")
+		
+	}
+
+	response = requests.post(url, data=payload, headers=headers)
+	response_json = response.json()
+	if (response_json.error):
+		return False
+	frappe.db.set_value('Settings', settings.name, 'authorization_code', response_json)
+
 	pass
