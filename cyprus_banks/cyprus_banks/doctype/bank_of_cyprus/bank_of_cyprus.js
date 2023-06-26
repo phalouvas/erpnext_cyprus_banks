7ba44e0cb251a7f4be286ff44331f8ab090f59ee
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-06-26 11:42:53 +0000

    trying to authorize to bank of cyprus

--- cyprus_banks/cyprus_banks/doctype/bank_of_cyprus/bank_of_cyprus.js
+++ cyprus_banks/cyprus_banks/doctype/bank_of_cyprus/bank_of_cyprus.js
@@ -2,7 +2,79 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Bank Of Cyprus', {
-	// refresh: function(frm) {
+	refresh: function (frm) {
 
-	// }
+		frm.add_custom_button(__('Authorize'), function () {
+			let urlParams = new URLSearchParams(window.location.search);
+			if (urlParams.get('state') === null) {
+				let is_sandbox = frm.get_field('is_sandbox').value;
+				let base_url = is_sandbox ? "https://sandbox-apis.bankofcyprus.com/df-boc-org-sb/sb/psd2/oauth2/authorize" : "https://apis.bankofcyprus.com/df-boc-org-prd/prod/psd2/oauth2/authorize";
+				let client_id = frm.get_field('client_id').value;
+				let href = base_url + "?response_type=code&client_id=" + client_id +
+					"&redirect_uri=" + window.location.href +
+					"&scope=TPPOAuth2Security" +
+					"&state=erpnext_state_b64_encoded";
+				window.location.href = href;
+				frappe.validated = true;
+			}
+		});
+
+		frm.add_custom_button(__('Create Accounts'), function () {
+			frappe.confirm('Are you sure you want to proceed?', function () {
+				frappe.call({
+					method: "cyprus_banks.cyprus_banks.doctype.hellenic_bank.hellenic_bank.create_accounts",
+					args: {
+						// your arguments here
+					},
+					callback: function (response) {
+						if (response.message.errors === null) {
+							frappe.msgprint("You succesfully created the bank accounts.");
+						} else {
+							frappe.msgprint("Something went wrong.", 'Error');
+						}
+					}
+				});
+			}, function () {
+				// action to perform if No is selected
+			});
+		});
+
+		let urlParams = new URLSearchParams(window.location.search);
+		let new_code = urlParams.get('code');
+		if (new_code !== null) {
+			frappe.db.get_single_value('Bank Of Cyprus', 'code')
+				.then(function (old_code) {
+					if ((urlParams.get('state') === "erpnext_state_b64_encoded") && (new_code !== old_code)) {
+						frappe.db.set_value('Bank Of Cyprus', '', 'code', urlParams.get('code'))
+							.then(r => {
+								let doc = r.message;
+								frappe.call({
+									method: "cyprus_banks.cyprus_banks.doctype.hellenic_bank.hellenic_bank.get_authorization_code",
+									args: {
+										// your arguments here
+									},
+									callback: function (response) {
+										if ('error' in response.message) {
+											frappe.msgprint(response.message.error);
+										} else {
+											frappe.msgprint("You succesfully received a new authorization code.");
+										}
+									}
+								});
+							})
+					}
+				});
+		}
+
+	},
+	onload: function (frm) {
+		frm.set_query('parent_account', function (doc) {
+			return {
+				filters: {
+					"is_group": 1,
+					"company": doc.company
+				}
+			};
+		});
+	}
 });
