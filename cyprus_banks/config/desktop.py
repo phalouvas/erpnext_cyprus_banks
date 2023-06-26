84662eb8cd6867f6b0ee8603bbc668669c213a1f
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-06-26 03:34:21 +0000

    renaming

--- cyprus_banks/config/desktop.py
+++ cyprus_banks/config/desktop.py
@@ -3,8 +3,8 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"module_name": "Erpnext Cyprus Banks",
+			"module_name": "Cyprus Banks",
 			"type": "module",
-			"label": _("Erpnext Cyprus Banks")
+			"label": _("Cyprus Banks")
 		}
 	]
