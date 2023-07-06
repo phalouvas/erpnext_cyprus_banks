796650128e988730bcde88af0014c45a4c485793
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-07-06 06:29:48 +0000

    asdf

--- cyprus_banks/cyprus_banks/doctype/hellenic_bank/hellenic_bank.py
+++ cyprus_banks/cyprus_banks/doctype/hellenic_bank/hellenic_bank.py
@@ -41,8 +41,7 @@ def get_authorization_code():
 		"Authorization": "Basic " + base64.b64encode(string_to_encode.encode("utf-8")).decode("utf-8")
 	}
 
-	print(payload)
-	print(headers)
+	frappe.throw("Basic " + base64.b64encode(string_to_encode.encode("utf-8")).decode("utf-8"))
 
 	response = requests.post(url, data=payload, headers=headers)
 	if (response.status_code != 200):
